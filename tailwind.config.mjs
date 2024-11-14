e2d20ac6ba702d7bf65b09c616befaaab028f98c
aftab <maftab1s@semo.edu>
2024-11-14 13:30:48 -0600

    ver final.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  integrations: [tailwind()],
 });
